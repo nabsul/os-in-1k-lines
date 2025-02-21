7402c3feeae2c1115bdcf7c08fa4a2e721f2e8c1
Nabeel S <11143071+nabsul@users.noreply.github.com>
2025-02-21 08:28:43 -0800

    Finished the exceptions chapter

--- run.sh
+++ run.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 set -xue
 
-# QEMU file path
 QEMU=qemu-system-riscv32
+CC=/opt/homebrew/opt/llvm/bin/clang
 
-# Start QEMU
-$QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot
+CFLAGS="-std=c11 -O2 -g3 -Wall -Wextra --target=riscv32-unknown-elf -fno-stack-protector -ffreestanding -nostdlib"
+
+# Build the kernel.
+$CC $CFLAGS -Wl,-Tkernel.ld -Wl,-Map=kernel.map -o kernel.elf kernel.c common.c
+
+$QEMU -machine virt -bios default -nographic -serial mon:stdio --no-reboot -kernel kernel.elf
